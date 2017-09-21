45a9d16ce58ea037f68dc65eb258f36cc0e2ef77
sunqi1993 <156689004@qq.com>
2017-09-21 14:25:23 +0800

    修改.gdbinit和IDE部分设置

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "key.h"
 #include "usart.h"
 #include "wdg.h"
-
+#include "lcd.h"
 #define en_uartRx_irq 0
 
 
@@ -292,10 +292,53 @@ void test_adc()
     delay_init();
 }
 
+void test_lcd(void)
+{
+    u8 x=0;
+    u8 lcd_id[12];			//存放LCD ID字符串
+    delay_init();	    	 //延时函数初始化
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);	 //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
+    uart_init(115200);	 	//串口初始化为115200
+    LED_Init();			     //LED端口初始化
+    LCD_Init();
+    POINT_COLOR=RED;
+    sprintf((char*)lcd_id,"LCD ID:%04X",lcddev.id);//将LCD ID打印到lcd_id数组。
+    while(1)
+    {
+        switch(x)
+        {
+            case 0:LCD_Clear(WHITE);break;
+            case 1:LCD_Clear(BLACK);break;
+            case 2:LCD_Clear(BLUE);break;
+            case 3:LCD_Clear(RED);break;
+            case 4:LCD_Clear(MAGENTA);break;
+            case 5:LCD_Clear(GREEN);break;
+            case 6:LCD_Clear(CYAN);break;
+
+            case 7:LCD_Clear(YELLOW);break;
+            case 8:LCD_Clear(BRRED);break;
+            case 9:LCD_Clear(GRAY);break;
+            case 10:LCD_Clear(LGRAY);break;
+            case 11:LCD_Clear(BROWN);break;
+        }
+        POINT_COLOR=RED;
+        LCD_ShowString(30,40,210,24,24,"WarShip STM32 ^_^");
+        LCD_ShowString(30,70,200,16,16,"TFTLCD TEST");
+        LCD_ShowString(30,90,200,16,16,"ATOM@ALIENTEK");
+        LCD_ShowString(30,110,200,16,16,lcd_id);		//显示LCD ID
+        LCD_ShowString(30,130,200,12,12,"2014/5/4");
+        x++;
+        if(x==12)x=0;
+        LED0=!LED0;
+        delay_ms(1000);
+
+    }
+}
 int  main(void)
 {
-    test_timer3();
+//    test_timer3();
 
+    test_lcd();
     return 1;
 
 }
